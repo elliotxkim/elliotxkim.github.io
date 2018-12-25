d150f4f67904789544e4202ac1afb26a52e6328e
Elliot Kim <elliotkim@elliots-mbp.lan>
2018-12-25 03:00:46 -0800

    Updates

---
+++ precache-manifest.7dd274220ccfffa466306a36fe6871ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/elliotxkim.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b2234fa4f16c3ce8e64e",
+    "url": "/elliotxkim.github.io/static/js/runtime~main.b2234fa4.js"
+  },
+  {
+    "revision": "55f2e0631ff2d031ab0b",
+    "url": "/elliotxkim.github.io/static/js/main.55f2e063.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/elliotxkim.github.io/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "55f2e0631ff2d031ab0b",
+    "url": "/elliotxkim.github.io/static/css/main.a411c5be.chunk.css"
+  },
+  {
+    "revision": "5f6bdae8aff3372bfe538e5a75d4e69a",
+    "url": "/elliotxkim.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49653d8e1ca25a53496291028ded43ba.js"
+  "/elliotxkim.github.io/precache-manifest.7dd274220ccfffa466306a36fe6871ba.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/elliotxkim.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
